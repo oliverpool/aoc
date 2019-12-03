ac131bf3bec692bf3db81479dcde82046c481433
oliverpool <oliverpool@hotmail.fr>
2019-12-03 21:43:04 +0100

    d01**

--- 01/main_test.go
+++ 01/main_test.go
@@ -13,6 +13,16 @@ func requiredFuel(mass int) int {
 	return mass/3 - 2
 }
 
+func requiredFuelCompenssated(mass int) int {
+	fuel := requiredFuel(mass)
+	sum := 0
+	for fuel > 0 {
+		sum += fuel
+		fuel = requiredFuel(fuel)
+	}
+	return sum
+}
+
 func TestRequiredFuel(t *testing.T) {
 	cc := []struct {
 		mass int
@@ -47,4 +57,40 @@ func TestFirst(t *testing.T) {
 	}
 	a.NoError(scanner.Err())
 	a.Equal(3490763, sum)
+}
+
+func TestRequiredFuelCompenssated(t *testing.T) {
+	cc := []struct {
+		mass int
+		fuel int
+	}{
+		{12, 2},
+		{14, 2},
+		{1969, 966},
+		{100756, 50346},
+	}
+	for _, c := range cc {
+		t.Run("Mass "+strconv.Itoa(c.mass), func(t *testing.T) {
+			a := assert.New(t)
+			a.Equal(c.fuel, requiredFuelCompenssated(c.mass))
+		})
+	}
+}
+
+func TestSecond(t *testing.T) {
+	a := assert.New(t)
+	f, err := os.Open("./input")
+	a.NoError(err)
+	defer f.Close()
+
+	sum := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		text := scanner.Text()
+		mass, err := strconv.Atoi(text)
+		a.NoError(err)
+		sum += requiredFuelCompenssated(mass)
+	}
+	a.NoError(scanner.Err())
+	a.Equal(5233250, sum)
 }
