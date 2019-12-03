0a9351dd7a27497065f8cfb630feefb43f681243
oliverpool <oliverpool@hotmail.fr>
2019-12-03 21:35:42 +0100

    d01*

---
+++ 01/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func requiredFuel(mass int) int {
+	return mass/3 - 2
+}
+
+func TestRequiredFuel(t *testing.T) {
+	cc := []struct {
+		mass int
+		fuel int
+	}{
+		{12, 2},
+		{14, 2},
+		{1969, 654},
+		{100756, 33583},
+	}
+	for _, c := range cc {
+		t.Run("Mass "+strconv.Itoa(c.mass), func(t *testing.T) {
+			a := assert.New(t)
+			a.Equal(c.fuel, requiredFuel(c.mass))
+		})
+	}
+}
+
+func TestFirst(t *testing.T) {
+	a := assert.New(t)
+	f, err := os.Open("./input")
+	a.NoError(err)
+	defer f.Close()
+
+	sum := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		text := scanner.Text()
+		mass, err := strconv.Atoi(text)
+		a.NoError(err)
+		sum += requiredFuel(mass)
+	}
+	a.NoError(scanner.Err())
+	a.Equal(3490763, sum)
+}
