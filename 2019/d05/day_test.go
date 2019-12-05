a20cbdbd8c0d8ddf7db9fffafd137c9395d3e783
oliverpool <oliverpool@hotmail.fr>
2019-12-05 09:11:52 +0100

    d05**

--- 2019/d05/day_test.go
+++ 2019/d05/day_test.go
@@ -55,6 +55,72 @@ func runProgram(opcodes []int, input int) ([]int, error) {
 			}
 			output = append(output, pos)
 			i += 2
+		case 5:
+			mode := opcodes[i] / 100
+			first := opcodes[i+1]
+			if mode%10 == 0 { // position mode
+				first = opcodes[first]
+			}
+			mode /= 10
+			second := opcodes[i+2]
+			if mode%10 == 0 { // position mode
+				second = opcodes[second]
+			}
+			if first != 0 {
+				i = second
+			} else {
+				i += 3
+			}
+		case 6:
+			mode := opcodes[i] / 100
+			first := opcodes[i+1]
+			if mode%10 == 0 { // position mode
+				first = opcodes[first]
+			}
+			mode /= 10
+			second := opcodes[i+2]
+			if mode%10 == 0 { // position mode
+				second = opcodes[second]
+			}
+			if first == 0 {
+				i = second
+			} else {
+				i += 3
+			}
+		case 7:
+			mode := opcodes[i] / 100
+			first := opcodes[i+1]
+			if mode%10 == 0 { // position mode
+				first = opcodes[first]
+			}
+			mode /= 10
+			second := opcodes[i+2]
+			if mode%10 == 0 { // position mode
+				second = opcodes[second]
+			}
+			if first < second {
+				opcodes[opcodes[i+3]] = 1
+			} else {
+				opcodes[opcodes[i+3]] = 0
+			}
+			i += 4
+		case 8:
+			mode := opcodes[i] / 100
+			first := opcodes[i+1]
+			if mode%10 == 0 { // position mode
+				first = opcodes[first]
+			}
+			mode /= 10
+			second := opcodes[i+2]
+			if mode%10 == 0 { // position mode
+				second = opcodes[second]
+			}
+			if first == second {
+				opcodes[opcodes[i+3]] = 1
+			} else {
+				opcodes[opcodes[i+3]] = 0
+			}
+			i += 4
 		case 99:
 			return output, nil
 		default:
@@ -128,5 +194,75 @@ func TestFirst(t *testing.T) {
 
 	out, err := runProgram(input, 1)
 	a.NoError(err)
-	a.Equal(3895705, out[len(out)-1])
+	a.Equal(4511442, out[len(out)-1])
+}
+
+func TestProgramJump(t *testing.T) {
+	longCase := []int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+		1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+		999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99}
+	cc := []struct {
+		input  []int
+		sdtin  int
+		sdtout int
+	}{
+		{[]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}, 7, 0},
+		{[]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}, 8, 1},
+		{[]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}, 9, 0},
+
+		{[]int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}, 7, 1},
+		{[]int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}, 8, 0},
+		{[]int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}, 9, 0},
+
+		{[]int{3, 3, 1108, -1, 8, 3, 4, 3, 99}, 7, 0},
+		{[]int{3, 3, 1108, -1, 8, 3, 4, 3, 99}, 8, 1},
+		{[]int{3, 3, 1108, -1, 8, 3, 4, 3, 99}, 9, 0},
+
+		{[]int{3, 3, 1107, -1, 8, 3, 4, 3, 99}, 7, 1},
+		{[]int{3, 3, 1107, -1, 8, 3, 4, 3, 99}, 8, 0},
+		{[]int{3, 3, 1107, -1, 8, 3, 4, 3, 99}, 9, 0},
+
+		{[]int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}, 0, 0},
+		{[]int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}, 1, 1},
+		{[]int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}, 2, 1},
+
+		{[]int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, 0, 0},
+		{[]int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, 1, 1},
+		{[]int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}, 2, 1},
+
+		{longCase, 6, 999},
+		{longCase, 7, 999},
+		{longCase, 8, 1000},
+		{longCase, 9, 1001},
+	}
+	for _, c := range cc {
+		t.Run("Pogram ", func(t *testing.T) {
+			a := assert.New(t)
+			out, err := runProgram(c.input, c.sdtin)
+			a.NoError(err)
+			a.Equal(c.sdtout, out[len(out)-1])
+		})
+	}
+}
+
+func TestSecond(t *testing.T) {
+	a := assert.New(t)
+	f, err := os.Open("./input")
+	a.NoError(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(SplitByte(','))
+	var input []int
+	for scanner.Scan() {
+		text := scanner.Text()
+		opcode, err := strconv.Atoi(strings.TrimSpace(text))
+		a.NoError(err)
+		input = append(input, opcode)
+	}
+	a.NoError(scanner.Err())
+
+	out, err := runProgram(input, 5)
+	a.NoError(err)
+	a.Equal(12648139, out[len(out)-1])
 }
