1d8611924f989214ca8bef7e1559faff2bee9eca
oliverpool <oliverpool@hotmail.fr>
2019-12-06 10:55:09 +0100

    d06**

--- 2019/d06/day_test.go
+++ 2019/d06/day_test.go
@@ -2,6 +2,7 @@ package d06
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -11,7 +12,6 @@ import (
 )
 
 func orbitSum(r io.Reader) (int, error) {
-
 	pairs := make(map[string]string)
 
 	scanner := bufio.NewScanner(r)
@@ -69,4 +69,70 @@ func TestFirst(t *testing.T) {
 	s, err := orbitSum(f)
 	a.NoError(err)
 	a.Equal(294191, s)
+}
+
+func orbitMinTransfer(r io.Reader) (int, error) {
+	pairs := make(map[string]string)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		parts := strings.SplitN(text, ")", 2)
+		pairs[parts[1]] = parts[0]
+	}
+
+	// find all YOU ancestors
+	current := pairs["YOU"]
+	youAncestors := map[string]int{current: 0}
+	for current != "COM" {
+		next := pairs[current]
+		youAncestors[next] = youAncestors[current] + 1
+		current = next
+	}
+
+	// find all SAN ancestors
+	current = pairs["SAN"]
+	sanAncestors := map[string]int{current: 0}
+	for current != "COM" {
+		if youDepth, ok := youAncestors[current]; ok {
+			return youDepth + sanAncestors[current], nil
+		}
+		next := pairs[current]
+		sanAncestors[next] = sanAncestors[current] + 1
+		current = next
+	}
+
+	return 0, fmt.Errorf("no possible transfer")
+}
+
+func TestSecondSample(t *testing.T) {
+	a := assert.New(t)
+	input := strings.NewReader(`COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`)
+	s, err := orbitMinTransfer(input)
+	a.NoError(err)
+	a.Equal(4, s)
+}
+
+func TestSecond(t *testing.T) {
+	a := assert.New(t)
+	f, err := os.Open("./input")
+	a.NoError(err)
+	defer f.Close()
+
+	s, err := orbitMinTransfer(f)
+	a.NoError(err)
+	a.Equal(424, s)
 }
