9a489ed1bef9990bd176d2a383e2e30ad8e71eb0
oliverpool <oliverpool@hotmail.fr>
2019-12-14 15:43:29 +0100

    d14*

---
+++ 2019/d14/day_test.go
@@ -0,0 +1,201 @@
+package d14
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type chemical string
+
+type reaction struct {
+	output int
+	input  map[chemical]int
+}
+
+func open(path string, cb func(io.Reader) error) error {
+	f, err := os.Open("./input")
+	if err != nil {
+		return err
+	}
+	return cb(f)
+}
+
+func parseInput(r io.Reader) (map[chemical]reaction, error) {
+	reactions := make(map[chemical]reaction)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+
+		text := strings.SplitN(scanner.Text(), " => ", 2)
+		inputs := strings.Split(text[0], ", ")
+
+		r := reaction{
+			input: make(map[chemical]int, len(inputs)),
+		}
+
+		for _, i := range inputs {
+			n, che, err := parseChemical(i)
+			if err != nil {
+				return reactions, err
+			}
+			r.input[che] = n
+		}
+
+		var err error
+		var che chemical
+		r.output, che, err = parseChemical(text[1])
+		if err != nil {
+			return reactions, err
+		}
+
+		reactions[che] = r
+	}
+	return reactions, scanner.Err()
+}
+
+func parseChemical(s string) (int, chemical, error) {
+	var n int
+	var che chemical
+	_, err := fmt.Sscanf(s, "%d %s", &n, &che)
+	return n, che, err
+}
+
+func TestParse(t *testing.T) {
+	cc := []struct {
+		input                string
+		count, subcount, ore int
+	}{
+		{
+			`10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL`,
+			6,
+			10,
+			31,
+		},
+		{
+			`9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL`,
+			7,
+			12,
+			165,
+		},
+	}
+	for _, c := range cc {
+		t.Run("", func(t *testing.T) {
+			a := assert.New(t)
+			m, err := parseInput(strings.NewReader(c.input))
+			a.NoError(err)
+			t.Log(m)
+			a.Equal(c.count, len(m))
+			subcount := 0
+			for _, r := range m {
+				subcount += len(r.input)
+			}
+			a.Equal(c.subcount, subcount)
+
+			d := computeDepth(m)
+			t.Log(d)
+			a.Equal(len(m)+1, len(d)) // + ORE
+
+			ore := reduceReaction(m, d)
+			a.Equal(c.ore, ore)
+		})
+	}
+}
+
+func computeDepth(r map[chemical]reaction) map[chemical]int {
+	reactions := make(map[chemical]reaction, len(r))
+	for k, v := range r {
+		reactions[k] = v
+	}
+
+	depths := map[chemical]int{"ORE": 0}
+	cDepth := func(cc map[chemical]int) int {
+		dmax := 0
+		for c := range cc {
+			d, ok := depths[c]
+			if !ok {
+				return -1
+			}
+			if d > dmax {
+				dmax = d
+			}
+		}
+		return dmax + 1
+	}
+	for len(reactions) > 0 {
+		for c, r := range reactions {
+			d := cDepth(r.input)
+			if d == -1 {
+				continue
+			}
+			depths[c] = d
+			delete(reactions, c)
+		}
+	}
+
+	return depths
+}
+
+func reduceReaction(reactions map[chemical]reaction, depths map[chemical]int) int {
+	maxDepth := func(cc map[chemical]int) int {
+		dmax := 0
+		for c := range cc {
+			d := depths[c]
+			if d > dmax {
+				dmax = d
+			}
+		}
+		return dmax
+	}
+	aggregate := reactions["FUEL"].input
+	for len(aggregate) > 1 {
+		d := maxDepth(aggregate)
+		for c, n := range aggregate {
+			if depths[c] != d {
+				continue
+			}
+			// need 'n' times the 'c' chemical
+			r := reactions[c]
+			// this reaction must happen 'factor' times
+			factor := ((n - 1) / r.output) + 1
+
+			for subc, subn := range r.input {
+				aggregate[subc] += factor * subn
+			}
+			delete(aggregate, c)
+		}
+	}
+	return aggregate["ORE"]
+}
+
+func TestFirst(t *testing.T) {
+	a := assert.New(t)
+	var reactions map[chemical]reaction
+	var err error
+	err = open("./input", func(r io.Reader) error {
+		reactions, err = parseInput(r)
+		return err
+	})
+	a.NoError(err)
+
+	depths := computeDepth(reactions)
+
+	ore := reduceReaction(reactions, depths)
+	a.Equal(205, ore)
+}
