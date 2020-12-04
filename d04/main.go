7a874332a6a48c5543479a03c34ab497c3e55291
oliverpool <git@olivier.pfad.fr>
2020-12-04 16:19:56 +0100

    :star: :star:

--- d04/main.go
+++ d04/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 type passport map[string]string
 
@@ -15,7 +19,6 @@ func (p passport) IsValid() bool {
 		//		"cid",
 	}
 	for _, field := range fields {
-		fmt.Println(field, p[field])
 		if p[field] == "" {
 			return false
 		}
@@ -28,6 +31,86 @@ func countValidPassports(pp []passport) int {
 	valid := 0
 	for _, p := range pp {
 		if p.IsValid() {
+			valid++
+		}
+	}
+	return valid
+}
+
+func parseInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+func (p passport) IsValidStrict() bool {
+	fields := map[string]func(string) bool{
+		"byr": func(s string) bool {
+			i := parseInt(s)
+			return i >= 1920 && i <= 2002
+		},
+		"iyr": func(s string) bool {
+			i := parseInt(s)
+			return i >= 2010 && i <= 2020
+		},
+		"eyr": func(s string) bool {
+			i := parseInt(s)
+			return i >= 2020 && i <= 2030
+		},
+		"hgt": func(s string) bool {
+			if len(s) < 3 {
+				return false
+			}
+			unit := s[len(s)-2:]
+			i := parseInt(s[:len(s)-2])
+			fmt.Println(unit, i)
+			if unit == "cm" {
+				return i >= 150 && i <= 193
+			}
+			if unit == "in" {
+				return i >= 59 && i <= 76
+			}
+			return false
+		},
+		"hcl": func(s string) bool {
+			m, err := regexp.MatchString(`^#[0-9a-f]{6}$`, s)
+			if err != nil {
+				panic(err)
+			}
+			return m
+		},
+		"ecl": func(s string) bool {
+			for _, e := range []string{
+				"amb", "blu", "brn", "gry", "grn", "hzl", "oth",
+			} {
+				if s == e {
+					return true
+				}
+			}
+			return false
+		},
+		"pid": func(s string) bool {
+			m, err := regexp.MatchString(`^[0-9]{9}$`, s)
+			if err != nil {
+				panic(err)
+			}
+			return m
+		},
+		//		"cid",
+	}
+	for field, valid := range fields {
+		if valid == nil {
+			continue
+		}
+		if !valid(p[field]) {
+			return false
+		}
+	}
+	return true
+
+}
+func countValidStrictPassports(pp []passport) int {
+	valid := 0
+	for _, p := range pp {
+		if p.IsValidStrict() {
 			valid++
 		}
 	}
--- d04/main_test.go
+++ d04/main_test.go
@@ -35,14 +35,43 @@ func TestFirst(t *testing.T) {
 }
 
 func TestSecond(t *testing.T) {
-	// forest := parseInput(example)
-	// require.Equal(t, 336, treeEncountersProduct(forest))
+	batch := parseInput(`eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
 
-	// input, err := ioutil.ReadFile("./input")
-	// require.NoError(t, err)
-	// forest = parseInput(string(input))
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
 
-	// require.Equal(t, 3492520200, treeEncountersProduct(forest))
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`)
+	require.Equal(t, 0, countValidStrictPassports(batch))
+
+	batch = parseInput(`pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`)
+	require.Equal(t, 4, countValidStrictPassports(batch))
+
+	batch = parseInput(example)
+	require.Equal(t, 2, countValidStrictPassports(batch))
+
+	input, err := ioutil.ReadFile("./input")
+	require.NoError(t, err)
+	batch = parseInput(string(input))
+
+	require.Equal(t, 237, countValidStrictPassports(batch))
 }
 func parseInput(s string) []passport {
 	ss := strings.Split(strings.TrimSpace(s), "\n\n")
