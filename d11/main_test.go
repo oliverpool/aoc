b0698241ab516a7e2178434a0ee1180b95da9347
oliverpool <git@olivier.pfad.fr>
2020-12-11 22:29:35 +0100

    :star: :star:

--- d11/main_test.go
+++ d11/main_test.go
@@ -36,6 +36,26 @@ func (c coord) neighboors() []coord {
 	}
 }
 
+func (c coord) sightNeighboors(m map[coord]byte) []coord {
+	var sn []coord
+	for i := -1; i < 2; i++ {
+		for j := -1; j < 2; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			var c2 coord
+			looking := true
+			for d := 1; looking; d++ {
+				c2 = coord{c.x + i*d, c.y + j*d}
+				looking = m[c2] == floor
+			}
+			//			fmt.Println(c2)
+			sn = append(sn, c2)
+		}
+	}
+	return sn
+}
+
 const (
 	floor  = '.'
 	empty  = 'L'
@@ -91,6 +111,40 @@ func countOccupied(m map[coord]byte) int {
 	return c
 }
 
+func stabilizeSight(m map[coord]byte) map[coord]byte {
+	sightNeighboors := make(map[coord][]coord)
+	for c := range m {
+		sightNeighboors[c] = c.sightNeighboors(m)
+	}
+
+	changed := true
+	for changed {
+		changed = false
+		neighboors := make(map[coord]int)
+		for c, v := range m {
+			if v != seated {
+				continue
+			}
+			for _, n := range sightNeighboors[c] {
+				neighboors[n]++
+			}
+		}
+		m2 := make(map[coord]byte)
+		for c, s := range m {
+			if s == empty && neighboors[c] == 0 {
+				m2[c] = seated
+				changed = true
+			} else if s == seated && neighboors[c] >= 5 {
+				m2[c] = empty
+				changed = true
+			} else {
+				m2[c] = s
+			}
+		}
+		m = m2
+	}
+	return m
+}
 func TestFirst(t *testing.T) {
 	layout := parseInput(example)
 	stab := stabilize(layout)
@@ -103,4 +157,18 @@ func TestFirst(t *testing.T) {
 	stab = stabilize(layout)
 	c = countOccupied(stab)
 	require.Equal(t, 2481, c)
+}
+
+func TestSecond(t *testing.T) {
+	layout := parseInput(example)
+	stab := stabilizeSight(layout)
+	c := countOccupied(stab)
+	require.Equal(t, 26, c)
+
+	input, err := ioutil.ReadFile("./input")
+	require.NoError(t, err)
+	layout = parseInput(string(input))
+	stab = stabilizeSight(layout)
+	c = countOccupied(stab)
+	require.Equal(t, 2227, c)
 }
