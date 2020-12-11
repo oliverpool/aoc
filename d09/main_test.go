8ac1237c0cd626309738e6fd262e240e0a5e9890
oliverpool <git@olivier.pfad.fr>
2020-12-11 21:32:37 +0100

    :star: :star:

--- d09/main_test.go
+++ d09/main_test.go
@@ -35,19 +35,23 @@ func TestFirst(t *testing.T) {
 	list := parseInput(example)
 	invalid := firstInvalid(list, 5)
 	require.Equal(t, 127, invalid)
+	min, max := contiguousSum(invalid, list)
+	require.Equal(t, 15, min)
+	require.Equal(t, 47, max)
 
 	input, err := ioutil.ReadFile("./input")
 	require.NoError(t, err)
 	list = parseInput(string(input))
 	invalid = firstInvalid(list, 25)
 	require.Equal(t, 776203571, invalid)
+	min, max = contiguousSum(invalid, list)
+	require.Equal(t, 104800569, min+max)
 }
 
 func firstInvalid(list []int, preamble int) int {
 	valid := make(map[int]int, preamble*(preamble-1)/2)
 	for i, v := range list[:preamble] {
 		for _, w := range list[i+1 : preamble] {
-			fmt.Println(v, w)
 			valid[v+w]++
 		}
 	}
@@ -63,6 +67,31 @@ func firstInvalid(list []int, preamble int) int {
 		}
 	}
 	return -1
+}
+
+func contiguousSum(sum int, list []int) (int, int) {
+	i, j := 0, 0
+	s := list[0]
+	for s != sum {
+		fmt.Println(i, j, s)
+		if s < sum {
+			j++
+			s += list[j]
+		} else {
+			s -= list[i]
+			i++
+		}
+	}
+	min, max := list[i], list[i]
+	for _, v := range list[i : j+1] {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return min, max
 }
 
 func parseInput(s string) []int {
